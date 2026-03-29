7304de5dc488607eed1c0ea955d3734e7d8484ab
jo-jo-frog <suskokirill45@gmail.com>
2026-03-29 20:52:57 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,10 +1,8 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-unsigned int faStr1(const char* str)
-{
-    if (!str)
-    {
+unsigned int faStr1(const char* str) {
+    if (!str) {
         return 0;
     }
 
@@ -13,23 +11,17 @@ unsigned int faStr1(const char* str)
     bool isWord = false;
     bool hasDigit = false;
 
-    for (size_t i = 0; i <= len; i++)
-    {
+    for (size_t i = 0; i <= len; i++) {
         char ch = str[i];
-        if (ch == ' ' || ch == '\0')
-        {
-            if (isWord && !hasDigit)
-            {
+        if (ch == ' ' || ch == '\0') {
+            if (isWord && !hasDigit) {
                 counter++;
             }
             isWord = false;
             hasDigit = false;
-        }
-        else
-        {
+        } else {
             isWord = true;
-            if (isdigit(static_cast<unsigned char>(ch)))
-            {
+            if (isdigit(static_cast<unsigned char>(ch))) {
                 hasDigit = true;
             }
         }
@@ -37,43 +29,32 @@ unsigned int faStr1(const char* str)
     return counter;
 }
 
-unsigned int faStr2(const char* str) 
-{
-    if (!str)
-    {
+unsigned int faStr2(const char* str) {
+    if (!str) {
         return 0;
     }
+
     int counter = 0;
     size_t len = strlen(str);
     bool firstChar = false;
     bool fail = false;
 
-    for (size_t i = 0; i <= len; i++)
-    {
+    for (size_t i = 0; i <= len; i++) {
         char ch = str[i];
-        if (ch == ' ' || ch == '\0')
-        {
-            if (!fail && firstChar)
-            {
+        if (ch == ' ' || ch == '\0') {
+            if (!fail && firstChar) {
                 counter++;
             }
             firstChar = false;
             fail = false;
-        }
-        else
-        {
-            if (!firstChar)
-            {
+        } else {
+            if (!firstChar) {
                 firstChar = true;
-                if (!(ch >= 'A' && ch <= 'Z'))
-                {
+                if (!(ch >= 'A' && ch <= 'Z')) {
                     fail = true;
                 }
-            }
-            else
-            {
-                if (!(ch >= 'a' && ch <= 'z'))
-                {
+            } else {
+                if (!(ch >= 'a' && ch <= 'z')) {
                     fail = true;
                 }
             }
@@ -82,10 +63,8 @@ unsigned int faStr2(const char* str)
     return counter;
 }
 
-unsigned int faStr3(const char* str) 
-{
-    if (!str)
-    {
+unsigned int faStr3(const char* str) {
+    if (!str) {
         return 0;
     }
 
@@ -94,28 +73,18 @@ unsigned int faStr3(const char* str)
     int Wordcounter = 0;
     unsigned answer = 0;
 
-    for (size_t i = 0; i <= len; i++)
-    {
+    for (size_t i = 0; i <= len; i++) {
         char ch = str[i];
-        if (ch == '\0')
-        {
-            if (Wordcounter != 0)
-            {
+        if (ch == '\0') {
+            if (Wordcounter != 0) {
                 answer = (length + Wordcounter / 2) / Wordcounter;
-            }
-            else
-            {
+            } else {
                 return 0;
             }
-        }
-        else
-        {
-            if (ch != ' ')
-            {
+        } else {
+            if (ch != ' ') {
                 length++;
-            }
-            else
-            {
+            } else {
                 Wordcounter++;
             }
         }
